c9e397a2b5c649a09698dc205a520b0d2c38411b
Tian99 <jit29@pitt.edu>
2020-03-03 14:58:30 -0500

    Getting started

---
+++ excel_formatting.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pandas import DataFrame as df
+
+
+def split(content, lis):
+
+	try:
+		content = content.split("\\")
+	except AttributeError:
+		for i in range(0, len(lis)):
+			lis[i].append('NULL')
+		return lis
+	#Just adjusting the format:
+	content[0] = content[0][1:]
+	for i in range(0, len(content)):
+		lis[i].append(content[i])
+
+	return lis
+
+def format_assign(raw):
+
+	count = 0
+	lis = [[] for i in range(0, 7)]
+	d = {}
+
+	for index, row in raw.iterrows():
+		content = row['AALNAMECONCATENATED']
+		lis = split(content, lis)
+		print(count/len(raw))
+		count += 1
+	print('finish spliting')
+
+	for i in range(1, len(lis)):
+		 d["loc_{0}".format(i)]=lis[i]
+	print('finish formatting')
+
+	new = df(d)
+	raw = pd.concat([raw, new], axis = 1)
+	#Combine two columns 
+	raw['loc_4+5'] = raw['loc_4'] + '\\' + raw['loc_5']
+	print('finish assigning')
+
+	#Sort the dataframe
+	raw.sort_values(by=['PARTNO'], inplace=True)
+	print('finish sorting')
+
+	raw.drop_duplicates(subset=['PARTNO', 'AALNAMECONCATENATED'], keep=False, inplace = True)
+	print('Duplicates dropped')
+
+	return raw
+
+
+
---
+++ map_construct.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from pandas import DataFrame as df
+
+def construct(raw):
+	big_map = {}
+	location_2_c = {}
+	location_3_c = {}
+	location_4_c = {}
+	location_5_c = {}
+	location_5_c = {}
+	#First get the instance of all the first level locations
+	#Location_1 is a scam, starting with location_2
+	location_1_c = raw['loc_2'].unique()
+	every_2 = raw['loc_3'].unique()
+	every_3 = raw['loc_4'].unique()
+	every_4 = raw['loc_5'].unique()
+
+	#This is aligned with the respect to the location above
+	for i in location_1_c:
+		location_2_c[i] = (raw.loc[raw['loc_2'] == i]['loc_3'].unique())
+
+	for i in every_2:
+		location_3_c[i] = (raw.loc[raw['loc_3'] == i]['loc_4'].unique())
+
+	for i in every_3:
+		location_4_c[i] = (raw.loc[raw['loc_4'] == i]['loc_5'].unique())
+
+	for i in every_4:
+		location_5_c[i] = (raw.loc[raw['loc_5'] == i]['loc_6'].unique())
+
+
+
+	print(len(location_2_c))
+	print(len(location_1_c))
+	print(len(location_3_c))
+	print(len(location_4_c))
+	print(len(location_5_c))
+
+
+
+
+
